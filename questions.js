9c1d533a3bf4b5f9a3f36d57ed738c7cf997bf49
Creed Mangrum <creed.mangrum@gmail.com>
2017-07-01 15:45:32 -0400

    initial

---
+++ questions.js
@@ -0,0 +1,382 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_GB" : [
+        {
+            "Where is Legolas from?": [
+                "The Greenwood",
+                "Fang orn Forest",
+                "Lothlorien",
+                "Rivendell"
+                
+            ]
+        },
+        {
+            "Who is the father of Legolas? ": [
+                "Thranduil",
+                "Elrond",
+                "Keleborn",
+                "Thorin"
+            ]
+        },
+        {
+            "Where do Frodo and Sam first encounter Faramir?": [
+                "Ithilien",
+                "Gondor",
+                "Row han",
+                "Osgiliath"
+            ]
+        },
+        {
+            "What is the name of the meeting to which Treebeard calls the Ents? ": [
+                "Entmoot",
+                "Tree Meet",
+                "Ent Song",
+                "Council of Fang orn"
+            ]
+        },
+        {
+            "Who possesses the mind of Theoden, king of Row han?": [
+                "Sarum on",
+                "Sauron",
+                "Wormtongue",
+                "Gand olf"
+            ]
+        },
+        {
+            "What is the name of the crystal ball used by Sarum on to communicate with Sauron?": [
+                "Palantir",
+                "Pensieve",
+                "Mirror of Galadriel",
+                "The Looking Glass"
+            ]
+        },
+        {
+            "How many palantiri exist in Middle Earth?": [
+                "7",
+                "3",
+                "9",
+                "1"
+            ]
+        },
+        {
+            "How many rings of power were given to the Dwarves?": [
+                "7",
+                "3",
+                "9",
+                "1"
+            ]
+        },
+        {
+            "What light is contained in the phial given to Frodo by Galadriel?": [
+                "Light of Earendil",
+                "Light of Lothlorien",
+                "White light",
+                "The heart of Elrond"
+            ]
+        },
+        {
+            "Where is the Last Homely House East of the Sea?": [
+                "Rivendell",
+                "Hobbitan",
+                "Bree",
+                "Row han"
+            ]
+        },
+        {
+            "What strange singing man welcomes Frodo, Sam, Merry, and Pippin into his home in the Old Forest?": [
+                "Tom Bombadil",
+                "Bayorn",
+                "Butterbur",
+                "Elrond"
+            ]
+        },
+        {
+            "What is the name of the Undying Lands to which the Elves go when leaving Middle Earth?": [
+                "Valinor",
+                "Rivendell",
+                "Lothlorien",
+                "Grey Havens"
+            ]
+        },
+        {
+            "What is the name of the shipwright who dwells at the Grey Havens, waiting until the last ship sails?": [
+                "Kirdan",
+                "Boromir",
+                "Kelebrimbor",
+                "Kurunir"
+            ]
+        },
+        {
+            "What is the elvish name for the wizards of Middle Earth?": [
+                "Estari",
+                "Nahzgul",
+                "Meddlers",
+                "Wargs"
+            ]
+        },
+        {
+            "How many wizards came to Middle Earth?": [
+                "5",
+                "3",
+                "1",
+                "7"
+            ]
+        },
+        {
+            "What is the name of the race of men from which Aragorn is descended?": [
+                "doonuh dine",
+                "numeh norian",
+                "Noldorin",
+                "elen deel"
+            ]
+        },
+        {
+            "What is the name of the beast pulled Gandalf into the pits of Moria?": [
+                "Balrog",
+                "Orc",
+                "Warg",
+                "Nahzgul"
+            ]
+        },
+        {
+            "Who created the Balrogs?": [
+                "Morgoth",
+                "Sauron",
+                "Sarum on",
+                "The Nahzgul"
+            ]
+        },
+        {
+            "How old is Bilbo at his great birthday party?": [
+                "111",
+                "81",
+                "100",
+                "50"
+            ]
+        },
+        {
+            "What color Pilgrim is Gand olf called?": [
+                "Grey",
+                "White",
+                "Blue",
+                "Brown"
+            ]
+        },
+        {
+            "What does Samwise call his father?": [
+                "Gaffer",
+                "Papa",
+                "Old man",
+                "The man I never knew"
+            ]
+        },
+        {
+            "Who is Frodo\’s uncle?": [
+                "Bilbo",
+                "Samwise",
+                "Lotho Baggins",
+                "Gimli"
+            ]
+        },
+        {
+            "Who does Aragorn put in charge of the Shire once he becomes king?": [
+                "Nobody",
+                "Gand olf",
+                "Frodo",
+                "Himself"
+            ]
+        }
+    ],
+    "QUESTIONS_EN_US" : [
+        {
+            "Where is Legolas from?": [
+                "The Greenwood",
+                "Fang orn Forest",
+                "Lothlorien",
+                "Rivendell",
+                "Ithilien",
+            ]
+        },
+        {
+            "Who is the father of Legolas? ": [
+                "Thranduil",
+                "Elrond",
+                "Keleborn",
+                "Thorin"
+            ]
+        },
+        {
+            "Where do Frodo and Sam first encounter Faramir?": [
+                "Ithilien",
+                "Gondor",
+                "Row han",
+                "Osgiliath"
+            ]
+        },
+        {
+            "What is the name of the meeting to which Treebeard calls the Ents? ": [
+                "Entmoot",
+                "Tree Meet",
+                "Ent Song",
+                "Council of Fang orn"
+            ]
+        },
+        {
+            "Who possesses the mind of Theoden, king of Row han?": [
+                "Sarum on",
+                "Sauron",
+                "Wormtongue",
+                "Gand olf"
+            ]
+        },
+        {
+            "What is the name of the crystal ball used by Sarum on to communicate with Sauron?": [
+                "Palantir",
+                "Pensieve",
+                "Mirror of Galadriel",
+                "The Looking Glass"
+            ]
+        },
+        {
+            "How many palantiri exist in Middle Earth?": [
+                "7",
+                "3",
+                "9",
+                "1"
+            ]
+        },
+        {
+            "How many rings of power were given to the Dwarves?": [
+                "7",
+                "3",
+                "9",
+                "1"
+            ]
+        },
+        {
+            "What light is contained in the phial given to Frodo by Galadriel?": [
+                "Light of Earendil",
+                "Light of Lothlorien",
+                "White light",
+                "The heart of Elrond"
+            ]
+        },
+        {
+            "Where is the Last Homely House East of the Sea?": [
+                "Rivendell",
+                "Hobbitan",
+                "Bree",
+                "Row han"
+            ]
+        },
+        {
+            "What strange singing man welcomes Frodo, Sam, Merry, and Pippin into his home in the Old Forest?": [
+                "Tom Bombadil",
+                "Bayorn",
+                "Butterbur",
+                "Elrond"
+            ]
+        },
+        {
+            "What is the name of the Undying Lands to which the Elves go when leaving Middle Earth?": [
+                "Valinor",
+                "Rivendell",
+                "Lothlorien",
+                "Grey Havens"
+            ]
+        },
+        {
+            "What is the name of the shipwright who dwells at the Grey Havens, waiting until the last ship sails?": [
+                "Kirdan",
+                "Boromir",
+                "Kelebrimbor",
+                "Kurunir"
+            ]
+        },
+        {
+            "What is the elvish name for the wizards of Middle Earth?": [
+                "Estari",
+                "Nahzgul",
+                "Meddlers",
+                "Wargs"
+            ]
+        },
+        {
+            "How many wizards came to Middle Earth?": [
+                "5",
+                "3",
+                "1",
+                "7"
+            ]
+        },
+        {
+            "What is the name of the race of men from which Aragorn is descended?": [
+                "doonuh dine",
+                "numeh norian",
+                "Noldorin",
+                "Ellen deal"
+            ]
+        },
+        {
+            "What is the name of the beast pulled Gandalf into the pits of Moria?": [
+                "Balrog",
+                "Orc",
+                "Warg",
+                "Nahzgul"
+            ]
+        },
+        {
+            "Who created the Balrogs?": [
+                "Morgoth",
+                "Sauron",
+                "Sarum on",
+                "The Nahzgul"
+            ]
+        },
+        {
+            "How old is Bilbo at his great birthday party?": [
+                "111",
+                "81",
+                "100",
+                "50"
+            ]
+        },
+        {
+            "What color Pilgrim is Gand olf called?": [
+                "Grey",
+                "White",
+                "Blue",
+                "Brown"
+            ]
+        },
+        {
+            "What does Samwise call his father?": [
+                "Gaffer",
+                "Papa",
+                "Old man",
+                "The man I never knew"
+            ]
+        },
+        {
+            "Who is Frodo\’s uncle?": [
+                "Bilbo",
+                "Samwise",
+                "Lotho Baggins",
+                "Gimli"
+            ]
+        },
+        {
+            "Who does Aragorn put in charge of the Shire once he becomes king?": [
+                "Nobody",
+                "Gand olf",
+                "Frodo",
+                "Himself"
+            ]
+        }
+    ]
+};
